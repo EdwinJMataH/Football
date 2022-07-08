53270d933d3283eb48d5a781e67ef75e3429f0d8
EdwinJMataH <ejmh.developer@gmail.com>
2022-07-08 15:01:29 -0500

    :sparkles: :lipstick: HTML structure and styles for main articles, contact section and footer

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 //Gulp
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
 
 
-const sass = require('gulp-sass')(require('sass'));
-const plumber = require('gulp-plumber');
+const sass     = require('gulp-sass')(require('sass'));
+const plumber  = require('gulp-plumber');
+
+//Imagenes
+const webp     = require('gulp-webp');
+const avif     = require('gulp-avif');
+const cache    = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
 
 function css(callback) {
     src('src/scss/**/*.scss') //Identificar el archivo sass
@@ -14,10 +20,48 @@ function css(callback) {
     callback();
 }
 
+function images (callback) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+
+    src('src/img/**/*.{png,jpg,jpeg}')
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest('build/img'));
+
+    callback();
+}
+
+function convertWebp (callback) {
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg,jpeg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'));
+    callback();
+}
+
+function convertAvif (callback) {
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg,jpeg}')
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'));
+
+    callback();
+}
+
 function dev(callback) {
     watch('src/scss/**/*.scss',css);
     callback();
 }
 
-exports.dev = dev;
+exports.images = images;
+exports.convertWebp = convertWebp;
+exports.convertAvif = convertAvif;
+exports.dev = parallel(convertAvif, convertWebp, images, dev);
 
